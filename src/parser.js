9da387dd31ad945c90f6eeb3539271a15711c992
Mingyang Tong <TSS@mail.tmysam.top>
2024-03-11 12:58:21 +0800

    support Plaintext and Code

--- src/parser.js
+++ src/parser.js
@@ -358,39 +358,90 @@ class div {
     }
 }
 
-const codeRegExp = /\\CODE[\s\S]*?\\CODE/g;
-const allSequenceRegExp = /(\\[a-zA-Z-\\]+(\([\s\S]*?\))?)|(\$)|(`)/g;
+const codeRegExp = /^\\CODE(\([a-zA-Z-]*\))?$/g;
+const codeWithLanguageRegExp = /^\\CODE\([a-zA-Z-]+\)$/g;
+const allSequenceRegExp = /(\\[a-zA-Z-\\]+(\([\s\S]*?\))?)|(\${1,2})|(`)/g;
 export class essayCodeParser {
+    root = null;
+    currentFontStyle = new fontStyle();
+    currentParagraph = null;
+    currentSpan = null;
+    currentDiv = null;
+    essayCode = "";
+    pushToDiv(paragraph_like) {
+        if (this.currentDiv == null) {
+            this.currentDiv = new div();
+            this.root.pushParagraph(this.currentDiv);
+        }
+        this.currentDiv.pushParagraph(paragraph_like);
+    }
+    pushToParagraph(span_like) {
+        if (this.currentParagraph == null) {
+            this.currentParagraph = new paragraph("justify");
+            this.pushToDiv(this.currentParagraph);
+        }
+        this.currentParagraph.pushSpan(span_like);
+    }
+    processCurrentSpan(spanBegin, spanEnd) {
+        if (spanBegin == spanEnd - 1) return;
+        if (/^\s*$/.test(this.essayCode[spanBegin])) return;
+        let spanContent = this.essayCode.substring(spanBegin, spanEnd);
+        let span = new span(spanContent, this.currentFontStyle.copy());
+        this.pushToParagraph(span);
+    }
+
     parse(essayCode) {
+        this.essayCode = essayCode;
+        this.root = new div();
         controlSequences = [];
-        let currentParagraph = null;
-        let currentSpan = null;
-        let currentDiv = null;
+        let spanBegin = 0;
         let isInCode = false;
-        let codeBegins = 0;
         let isInInlineCode = false;
-        let inlineCodeBegins = 0;
         let isInFormula = false;
         while ((result = allSequenceRegExp.exec(essayCode))) {
             if (isInCode) {
                 if (result[0] == "\\CODE") {
                     isInCode = false;
-                    let code = essayCode.substring(codeBegins, result.index);
-                    currentParagraph.content = code;
-                    currentParagraph = null;
+                    let code = essayCode.substring(spanBegin, result.index);
+                    this.currentParagraph.content = code;
+                    this.currentParagraph = null;
                 }
+                spanBegin = result.index + result[0].length;
                 continue;
             }
             if (isInInlineCode) {
                 if (result[0] == "`") {
                     isInInlineCode = false;
-                    let code = essayCode.substring(
-                        inlineCodeBegins,
-                        result.index
-                    );
-                    currentSpan.content = code;
-                    currentSpan = null;
+                    let code = essayCode.substring(spanBegin, result.index);
+                    this.currentSpan.content = code;
+                    this.currentSpan = null;
                 }
+                spanBegin = result.index + result[0].length;
+                continue;
+            }
+            if (codeRegExp.test(result[0])) {
+                this.processCurrentSpan(spanBegin, result.index);
+                isInCode = true;
+                let codeStyle = defaultCodeStyle;
+                let language = "";
+                if (codeWithLanguageRegExp.test(result[0])) {
+                    language = result[0].substring(6, result[0].length - 1);
+                }
+                spanBegin = result.index + result[0].length;
+                this.currentParagraph = new blockCode(
+                    "code",
+                    codeStyle.language,
+                    ""
+                );
+                this.pushToDiv(this.currentParagraph);
+                continue;
+            }
+            if (result[0] == "`") {
+                this.processCurrentSpan(spanBegin, result.index);
+                isInInlineCode = true;
+                spanBegin = result.index + result[0].length;
+                this.currentSpan = new inlineCode("");
+                this.pushToParagraph(this.currentSpan);
                 continue;
             }
         }
